b9163733c7b972553572d5ea88b641f5805055a2
Sam <Sam@DESKTOP-2IHO4FC>
2022-02-12 01:37:32 +0200

    add tractor to the project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import { Tractor, Spacer, Typography } from '@aircall/tractor';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Tractor injectStyle>
+    <Spacer space="s">
+      <Typography variant="displayM">Hello</Typography>
+      <Typography variant="displayL">World</Typography>
+    </Spacer>
+  </Tractor>
+);
 
 export default App;
