87b31d4ecd4c799098f5c357d786d013414a0a97
Sam <Sam@DESKTOP-2IHO4FC>
2022-02-12 16:11:39 +0200

    add Eslint

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import { Tractor, Spacer, Typography } from '@aircall/tractor';
+import { Tractor, Spacer, Typography } from "@aircall/tractor";
 
-const App = () => (
-  <Tractor injectStyle>
-    <Spacer space="s">
-      <Typography variant="displayM">Hello</Typography>
-      <Typography variant="displayL">World</Typography>
-    </Spacer>
-  </Tractor>
-);
+function App() {
+  return (
+    <Tractor injectStyle>
+      <Spacer space="s">
+        <Typography variant="displayM">Hello</Typography>
+        <Typography variant="displayL">World</Typography>
+      </Spacer>
+    </Tractor>
+  );
+}
 
 export default App;
