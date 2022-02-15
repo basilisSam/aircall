ee4797a7c7d8a679eb00dae8ea879b7129eb6af1
Sam <Sam@DESKTOP-2IHO4FC>
2022-02-15 19:26:42 +0200

    add style to call card

--- src/componets/Call.js
+++ src/componets/Call.js
@@ -1,39 +1,70 @@
-import {ArchiveOutlined, Avatar, Box, Flex, IconButton, InformationOutlined, UserOutlined,} from "@aircall/tractor";
-import {Link} from "react-router-dom";
+import {
+  ArchiveOutlined,
+  Avatar,
+  Box,
+  CallFilled,
+  CallOutlined,
+  Flex,
+  IconButton,
+  InformationOutlined,
+  Spacer,
+  Typography,
+  UserOutlined,
+} from "@aircall/tractor";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
-const Call = ({call, updateCall}) => {
-    return (
-        <Box boxShadow={1} borderRadius={100} height='80px' width='500px' mb='15px'>
-            <Flex justifyContent='center' alignItems='flex-end'>
-                <Avatar size='large' icon={<UserOutlined/>}/>
-                {call.from}
-                {call.created_at}
-                <Link to={`/calls/${call.id}`}>
-                    <IconButton
-                        size={32}
-                        component={InformationOutlined}
-                        color='primary.base'
-                    />
-                </Link>
-                <IconButton
-                    onClick={() => updateCall(call.id)}
-                    size={32}
-                    component={ArchiveOutlined}
-                    color='red.base'
-                />
-            </Flex>
-        </Box>
-    );
+const Call = ({ call, updateCall }) => {
+  return (
+    <Box
+      boxShadow={1}
+      borderRadius={100}
+      height='80px'
+      width='350px'
+      mb='15px'
+      display={"flex"}
+    >
+      <Flex justifyContent='center' alignItems='center'>
+        <IconButton
+          size={32}
+          pr={30}
+          component={CallFilled}
+          color='primary.base'
+        />
+        <Spacer
+          space='s'
+          pr={30}
+          alignItems={"center"}
+          justifyContent={"center"}
+        >
+          <Typography variant='heading'>From:</Typography>
+          <Typography textAlign='right'> {call.from} </Typography>
+        </Spacer>
+        <Link to={`/calls/${call.id}`}>
+          <IconButton
+            size={32}
+            component={InformationOutlined}
+            color='primary.base'
+          />
+        </Link>
+        <IconButton
+          onClick={() => updateCall(call.id)}
+          size={32}
+          component={ArchiveOutlined}
+          color='red.base'
+        />
+      </Flex>
+    </Box>
+  );
 };
 
 Call.propTypes = {
-    call: PropTypes.shape({
-        from: PropTypes.string,
-        created_at: PropTypes.string,
-        id: PropTypes.string
-    }),
-    updateCall: PropTypes.func.isRequired
-}
+  call: PropTypes.shape({
+    from: PropTypes.string,
+    created_at: PropTypes.string,
+    id: PropTypes.string,
+  }),
+  updateCall: PropTypes.func.isRequired,
+};
 
 export default Call;
